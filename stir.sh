eb9ad5c8f1527976bb4e609caf364ad1f0a40781
Henric Jungheim <software@henric.org>
2015-06-10 18:17:40 -0700

    Handle several sites in parallel. Limit the number of sites per run to 25.

--- stir.sh
+++ stir.sh
@@ -2,17 +2,76 @@
 
 set -o pipefail
 
+print_cipher()
+{
+   local cipher=$(awk '/Cipher +:/ { print $3 }')
+
+   if [ ${#cipher} -eq 0 ] ; then
+      return 1
+   fi
+
+   printf "TLS %s (%s)\n" ${1} ${cipher} >/dev/stderr
+}
+
 tls()
 {
-   echo -n >/dev/stderr Connect to ${1}
-
    openssl s_client -connect ${1}:443 -no_ssl2 -no_ssl3 -CAfile /usr/local/etc/ssl/cert.pem < /dev/null 2>/dev/null \
-         | tee >(awk '/Cipher +:/ { print " (" $3 ")" }' >/dev/stderr ) \
+         | tee >( print_cipher ${1} ) \
       && return 0
 
-   echo >/dev/stderr " failed"
+   echo >/dev/stderr "${1} failed"
 
    exit 1
+}
+
+multi_tls()
+{
+   local pids idx sites=(${@})
+
+#echo >/dev/stderr multi_tls sites "${sites[@]}" indexes "${!sites[@]}"
+
+   local ret pid 
+
+   for idx in "${!sites[@]}" ; do
+      local site=${sites[${idx}]}
+
+      tls ${site} &
+      
+      ret=$?
+      pid=$! 
+
+      if [ ${ret} -ne 0 ] ; then
+         echo "Unable to read" ${site}
+         wait
+         exit 1
+      fi
+
+      pids[${idx}]=${pid}
+   done
+
+#echo > /dev/stderr pids ${pids[@]} sites ${sites[@]}
+
+   for idx in "${!sites[@]}" ; do
+      local pid=${pids[${idx}]}
+      local site=${sites[${idx}]}
+
+      if [ -z ${pid+x} ] ; then
+echo >/dev/stderr worker for ${site} has no pid
+         wait
+
+         exit 1 
+      fi
+
+#echo >/dev/stderr waiting for ${site} pid ${pid}
+      if ! wait ${pid} ; then
+echo >/dev/stderr worker for ${site} pid ${pid} failed
+         wait
+
+         exit 1
+      fi
+   done
+
+   return 0
 }
 
 _rng_generate_block()
@@ -29,9 +88,9 @@ _rng_generate_block()
 
 _rng_generate()
 {
-   _rng_rekey || return 1
+   _rng_rekey || exit 1
 
-   _rng_update || return 1
+   _rng_update || exit 1
 
    { \
       _rng_generate_block "generate" && \
@@ -47,21 +106,21 @@ _rng_update()
 {
    ((rng_count++))
 
-   rng=$( _rng_generate_block "rng" | base64 -e ) || return 1
+   rng=$( _rng_generate_block "rng" | base64 -e ) || exit 1
 
    ((rng_count++))
 }
 
 _rng_rekey()
 {
-   _rng_update || return 1
+   _rng_update || exit 1
 
    rng_key=$( \
       _rng_generate_block "key" \
       | openssl rand -rand /dev/stdin:/dev/random -hex 32 2> /dev/null \
-   ) || return 1
+   ) || exit 1
 
-   _rng_update || return 1
+   _rng_update || exit 1
 
    rng_iv=$( \
       _rng_generate_block "iv" \
@@ -72,17 +131,17 @@ _rng_rekey()
 # Stir the random pool (backtracking resistance)
 rng_stir()
 {
-   _rng_rekey || return 1
+   _rng_rekey || exit 1
 
    rng_pool=$( \
       { echo ${rng_pool} | base64 -d ; } \
       | openssl aes-256-cbc -e -K ${rng_key} -iv ${rng_iv} \
-      | base64 -e) || return 1
+      | base64 -e) || exit 1
 
    # Prevent accidental reuse
-   unset rng_key rng_iv || return 1
+   unset rng_key rng_iv || exit 1
   
-   _rng_update || return 1
+   _rng_update || exit 1
  
    _rng_generate_block "stir" \
       | openssl rand -rand /dev/stdin:/dev/random -hex 32 2>/dev/null >/dev/null
@@ -91,24 +150,58 @@ rng_stir()
 # Add entropy from given URL
 rng_add_tls()
 {
-   _rng_rekey || return 1
+   local pool code
 
-   # The TLS connection's master key is the important part
-   rng_pool=$( \
-      { tls ${1} && echo ${rng_pool} | base64 -d ; } \
-      | openssl aes-256-cbc -e -K ${rng_key} -iv ${rng_iv} \
-      | base64 -e) \
-   || return 1
+   for retry in {1..3} ; do
+      _rng_rekey || exit 1
 
-   # Prevent accidental reuse
-   unset rng_key rng_iv || return 1
+      # The TLS connection's master key is the important part
+      pool=$( \
+         { multi_tls "${@}" && echo ${rng_pool} | base64 -d ; } \
+         | openssl aes-256-cbc -e -K ${rng_key} -iv ${rng_iv} \
+         | base64 -e )
+
+      code=$?
+
+      # Prevent accidental reuse
+      unset rng_key rng_iv || exit 1
       
-   rng_stir
+      rng_stir || exit 1
+      
+      if [ ${code} -eq 0 ] ; then
+         break
+      fi
+
+      sleep 3
+      echo >/dev/stderr "Retrying"
+   done
+
+   if [ ${code} -ne 0 ] ; then
+      echo >/dev/stderr "Pool update failed"
+      exit 1
+   fi
+
+   rng_pool=${pool} 
+
+   return 0
+}
+
+rng_add_multi_tls()
+{
+   local all=(${@})
+
+   while [ ${#all[@]} -ne 0 ] ; do
+      local batch=("${all[@]:0:9}")
+
+      all=("${all[@]:9}")
+
+      rng_add_tls "${batch[@]}"
+   done
 }
 
 rng_generate()
 {
-   _rng_generate ${1} || return 1
+   _rng_generate ${1} || exit 1
 
   rng_stir
 }
@@ -135,19 +228,18 @@ ${anu_raw}"
 
    rng_stir || exit 1
 
-   local allSites split
+   local allSites
 
-   readarray -t allSites < <(sort -uR sites) || exit 1
+   readarray -t allSites < <(sort -uR sites | head -25) || exit 1
 
-   split=$(( 2 * ${#allSites[*]} / 3 ))
+   local split=$(( 2 * ${#allSites[*]} / 3 ))
 
    sites=("${allSites[@]:${split}}")
 
-   for site in "${allSites[@]:0:${split}}"
-   do
-      rng_add_tls ${site} || exit 1
-   done
- 
+   local firstSites=("${allSites[@]:0:${split}}")
+
+   rng_add_multi_tls "${firstSites[@]}"
+
    rng_stir || exit 1
 
 #echo >/dev/stderr pool length: ${#rng_pool}
@@ -155,16 +247,16 @@ ${anu_raw}"
 
 rng_tls()
 {
-   rng_keys || return 1
+   rng_keys || exit 1
 
-   rng_update || return 1
+   rng_update || exit 1
 
    keyed_rng_tls ${1} ${2} ${rng_key} ${rng_iv}
 }
 
 keyed_rng_tls()
 {
-   rng_update || return 1
+   rng_update || exit 1
 
    { \
       dd if=/dev/random bs=256 count=1 2>/dev/null && \
@@ -177,13 +269,11 @@ keyed_rng_tls()
 
 get_keys()
 {
-   iv=$(rng_generate 16) || return 1
+   iv=$(rng_generate 16) || exit 1
 
-   key=$(rng_generate 32) || return 1
+   key=$(rng_generate 32) || exit 1
 
-   stir=$(rng_generate 64) || return 1
-
-   return 0
+   stir=$(rng_generate 64) || exit 1
 }
 
 rng_initialize || exit 1
@@ -198,11 +288,8 @@ fi
 #echo >/dev/stderr stir is $stir
 
 rng_stir || exit 1
-  
-for site in "${sites[@]}"
-do
-   rng_add_tls ${site} || exit 1
-done
+
+rng_add_multi_tls "${sites[@]}"
 
 rng_stir || exit 1
 
